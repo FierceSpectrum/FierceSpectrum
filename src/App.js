224f7b23606198be475061b4fcd23138d9be934e
Benjamín Sandí <127576883+FierceSpectrum@users.noreply.github.com>
2024-08-29 21:08:07 -0600

    Update build

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const router = createHashRouter([
         element: (
           <>
             <Portafolio />
-            <Footer />
+            {/* <Footer /> */}
           </>
         ),
       },
