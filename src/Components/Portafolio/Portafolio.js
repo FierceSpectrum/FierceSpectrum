d659fe8bfb1e92110aa1d5c152b036a10c85224d
Benjamín Sandí <127576883+FierceSpectrum@users.noreply.github.com>
2024-08-29 21:20:04 -0600

    Update views

--- src/Components/Portafolio/Portafolio.js
+++ src/Components/Portafolio/Portafolio.js
@@ -44,8 +44,8 @@ const Portafolio = () => {
       <div className="portfolio-container">
         <WelcomeSection />
         <Skills />
-        {/* <FeaturedProjects />
-        <AboutMeSection /> */}
+        <FeaturedProjects />
+        <AboutMeSection />
       </div>
     </div>
   );
